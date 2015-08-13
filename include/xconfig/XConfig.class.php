a8a4a308e83f260442f9deaa98f57849f65b69ff
metalwolf <phil@thomassigny.org>
2015-08-13 11:19:24 -0400

    patch 1.1.0

--- include/xconfig/XConfig.class.php
+++ include/xconfig/XConfig.class.php
@@ -208,10 +208,14 @@ class XConfig implements ArrayAccess, Iterator, Countable
         $param = $line;
         $value = null;
       }
+      var_dump($param);
+      var_dump($value);
       if (in_array($value, array('true', 'yes', 'on'), true))
         $value = true;
       if (in_array($value, array('false', 'no', 'off', 'none'), true))
         $value = false;
+      var_dump($param);
+      var_dump($value);
       if (!isset($lines[$param]))
         $lines[$param] = $value;
       else
