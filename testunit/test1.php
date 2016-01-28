edd1b7c61abe379408989cf13ebf64e0e4cf975d
metalwolf <phil@thomassigny.org>
2016-01-28 18:34:51 -0500

    patch 2.0.1

--- testunit/test1.php
+++ testunit/test1.php
@@ -1,7 +1,13 @@
 <?php
 
-$x = new \xconfig\XConfig();
+class test1 extends PHPUnit_Framework_TestCase
+{
+  public function test1()
+  {
+    $x = new \xconfig\XConfig();
 
-var_dump($x);
+    var_dump($x);
+  }
+}
 
 ?>
